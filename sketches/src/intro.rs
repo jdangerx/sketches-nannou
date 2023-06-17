3911b06b21e7e055c8c889bb802e96a06fbb2bbb
Dazhong Xia <john.danger.xia@gmail.com>
2023-06-17 19:08:46 -0400

    Trying to recreate moon stuff

--- sketches/src/intro.rs
+++ sketches/src/intro.rs
@@ -9,7 +9,7 @@ fn main() {
 }
 
 struct Model {
-    my_box: Rect,
+    moon: Rect,
     texture: wgpu::Texture
 }
 
@@ -19,44 +19,33 @@ fn model(app: &App) -> Model {
     let texture = wgpu::Texture::from_path(app, img_path).unwrap();
 
     Model {
-        my_box: Rect::from_w_h(100.0, 100.0),
+        moon: Rect::from_w_h(200.0, 200.0),
         texture: texture
     }
 }
 
 fn update(app: &App, model: &mut Model, _update: Update) {
-    let win = app.window_rect();
-    let sine = app.time.sin();
-    let cosine = app.time.cos();
-
-    let boundary = win
-        .pad_left(model.my_box.w() / 2.0)
-        .pad_right(model.my_box.w() / 2.0)
-        .pad_top(model.my_box.h() / 2.0)
-        .pad_bottom(model.my_box.h() / 2.0)
-    ;
-
-    let x = map_range(cosine, -1.0, 1.0, boundary.left(), boundary.right());
-    let y = map_range(sine, -1.0, 1.0, boundary.top(), boundary.bottom());
-    let w = map_range(cosine, -1.0, 1.0, 50.0, 150.0);
-    let h = map_range(sine, -1.0, 1.0, 50.0, 150.0);
-    model.my_box = Rect::from_x_y_w_h(x, y, w, h);
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
     let win = app.window_rect();
     let draw = app.draw();
-    frame.clear(WHEAT);
+    frame.clear(MIDNIGHTBLUE);
 
-    draw.texture(&model.texture)
-        .xy(win.xy())
-        .wh(win.wh());
+    let half = Rect::from_w_h(model.moon.w() / 2.0, model.moon.h());
+    let left = draw.scissor(half.mid_left_of(model.moon));
+    let right = draw.scissor(half.mid_right_of(model.moon));
 
-    draw.rect()
-        .xy(model.my_box.xy())
-        .wh(model.my_box.wh())
-        .z_degrees(app.time * 10.0)
+    left.ellipse()
+        .wh(model.moon.wh())
+        .color(CORNSILK);
+
+    right.ellipse()
+        .wh(model.moon.wh())
         .color(SLATEBLUE);
+
+    // TODO (daz): how to clip the texture to only this shape?
+    left.texture(&model.texture);
 
     draw.to_frame(app, &frame).unwrap();
 }
